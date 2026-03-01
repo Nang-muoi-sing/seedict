4db4cf4269a29163530ff104789897054f34b63a
Tseing <im.yczeng@foxmail.com>
2026-03-01 17:15:10 +0800

    feat(utils): 添加 `Utterance` 简化注音解析流程

---
+++ src/components/common/YngpingSup.vue
@@ -0,0 +1,15 @@
+<template>
+  <template v-for="(syllable, index) in props.syllables" :key="index">
+    {{ syllable.value.initial }}{{ syllable.value.final
+    }}<sup>{{ syllable.value.tone }}</sup
+    ><template v-if="index != props.syllables.length - 1">&nbsp;</template>
+  </template>
+</template>
+
+<script setup lang="ts">
+import { Syllable } from '../../utils/phonetics';
+
+const props = defineProps<{
+  syllables: Syllable[];
+}>();
+</script>
