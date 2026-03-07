63660dd3b49be066684e71adbfcc823fe770576e
Tseing <im.yczeng@foxmail.com>
2026-03-07 15:38:53 +0800

    refactor: 修改 SeeToast SeeTooltip 为单例以解决性能问题

--- src/components/seeui/tooltip/SeeTooltip.vue
+++ src/components/seeui/tooltip/SeeTooltip.vue
@@ -4,7 +4,7 @@
     role="tooltip"
     class="tooltip invisible absolute z-50 inline-block rounded-xl bg-rosybrown-900 px-3 py-2 text-xs font-medium text-wheat-50 opacity-0 shadow-xl transition-opacity duration-300"
   >
-    <slot />
+    <div v-html="content"></div>
     <div class="tooltip-arrow" data-popper-arrow></div>
   </div>
 </template>
@@ -12,42 +12,42 @@
 <script setup lang="ts">
 import { onMounted, onUnmounted, ref } from 'vue';
 import { Tooltip } from 'flowbite';
-import type { TooltipOptions, TooltipInterface } from 'flowbite';
-
-interface Props {
-  id: string;
-  placement?: 'top' | 'bottom' | 'left' | 'right';
-  trigger?: 'hover' | 'click';
-}
-
-const props = withDefaults(defineProps<Props>(), {
-  placement: 'top',
-  trigger: 'hover',
-});
+import type { TooltipInterface } from 'flowbite';
 
 const targetEl = ref<HTMLElement | null>(null);
 const tooltipInstance = ref<TooltipInterface | null>(null);
+const content = ref('');
 
-onMounted(() => {
-  const $triggerEl = document.querySelector(
-    `[data-tooltip-target="${props.id}"]`
-  );
+const handleGlobalMouseOver = (e: MouseEvent) => {
+  // 寻找带有 data-tooltip-msg 的最近祖先
+  const triggerEl = (e.target as HTMLElement).closest('[data-tooltip-msg]');
 
-  if (targetEl.value && $triggerEl) {
-    const options: TooltipOptions = {
-      placement: props.placement,
-      triggerType: props.trigger,
-    };
+  if (triggerEl && targetEl.value) {
+    const msg = triggerEl.getAttribute('data-tooltip-msg') || '';
+    if (!msg) return;
 
+    content.value = msg;
+
+    tooltipInstance.value?.destroy();
     tooltipInstance.value = new Tooltip(
       targetEl.value,
-      $triggerEl as HTMLElement,
-      options
+      triggerEl as HTMLElement,
+      {
+        placement: 'top',
+        triggerType: 'hover',
+      }
     );
+
+    tooltipInstance.value.show();
   }
+};
+
+onMounted(() => {
+  window.addEventListener('mouseover', handleGlobalMouseOver);
 });
 
 onUnmounted(() => {
+  window.removeEventListener('mouseover', handleGlobalMouseOver);
   tooltipInstance.value?.destroy();
 });
 </script>
