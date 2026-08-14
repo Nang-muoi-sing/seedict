82d2d654534ac3e9b1e33f3a8f8ffbee2b047efb
Paul Sun <168397710+econsun@users.noreply.github.com>
2026-08-14 10:58:52 +0800

    docs: 完善网页文字
    优化 Windows、macOS、Linux 与 Android 的安装和挂载说明，补充安全提示、词库更新提醒及问题反馈入口。
    
    整理说明图片与注释的位置，并保持安装指南折叠按钮始终可见。

--- src/components/ImeDownloadStep.vue
+++ src/components/ImeDownloadStep.vue
@@ -1,34 +1,40 @@
 <template>
-  <SeeImage :src="githubOverviewImg" class="my-3 max-w-4xl" />
-  进入<Link href="https://github.com/Nang-muoi-sing/rime-hokchew/tree/release"
-    >配方仓库</Link
-  >，可以通过
-  <SeeKeycap label="Code" />
-  >
-  <SeeKeycap label="Download ZIP" />
-  下载压缩包，其中包含 3 种输入方案：
-  <ul class="list-disc pl-5">
-    <li>
-      <SeeCode code="hokchew" />
-      ：福州话汉字输入方案，输入榕拼打出福州话汉字。
-    </li>
-    <li>
-      <SeeCode code="hokchew_roma" />
-      ：福州话教会罗马字输入方案，输入教会罗马字转写打出教会罗马字。
-    </li>
-    <li>
-      <SeeCode code="hokchew_yngping" />
-      ：榕拼手写方案输入方案，输入榕拼打开榕拼手写形式。
-    </li>
-  </ul>
+  <div class="space-y-3">
+    <SeeImage :src="githubOverviewImg" class="max-w-4xl mx-auto" />
+    <div>
+      点击
+      <Link href="https://github.com/Nang-muoi-sing/rime-hokchew/tree/release"
+        >GitHub 链接</Link
+      >
+      进入配方仓库，可以通过
+      <SeeKeycap label="Code" />
+      >
+      <SeeKeycap label="Download ZIP" />
+      下载压缩包，其中包含 3 种输入方案：
+      <ul class="pl-5 list-disc">
+        <li>
+          <SeeCode code="hokchew" />
+          ：输入榕拼键入方案，打出福州话文字
+        </li>
+        <li>
+          <SeeCode code="hokchew_roma" />
+          ：输入教会罗马字转写，打出教会罗马字
+        </li>
+        <li>
+          <SeeCode code="hokchew_yngping" />
+          ：输入榕拼键入方案，打出榕拼手写方案
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
+import githubOverviewImg from '../assets/ime/github-overview.png';
 import Link from './common/Link.vue';
 import SeeKeycap from './seeui/keycap/SeeKeycap.vue';
-import SeeImage from './seeui/media/SeeImage.vue';
 import SeeCode from './seeui/code/SeeCode.vue';
-import githubOverviewImg from '../assets/ime/github-overview.png';
+import SeeImage from './seeui/media/SeeImage.vue';
 </script>
 
 <style scoped>
